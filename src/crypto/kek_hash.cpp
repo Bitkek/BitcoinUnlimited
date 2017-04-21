5e76efd3261deae4ce5b9ce9a439baa742a72d7c
MasterShadilay <administrator@jesbus.com>
2017-04-21 12:39:57 +0200

    Added some log messages

--- src/crypto/kek_hash.cpp
+++ src/crypto/kek_hash.cpp
@@ -15,16 +15,41 @@ void runKekHash(char* source, int length, char* destination)
 	sha256.Write((const unsigned char*)source, length);
 	sha256.Finalize((unsigned char*)destination);
 	
+	printf("Intermediate SHA256 is:\r\n");
+	for (int i=0; i<32; i++)
+	{
+		printf("%02X", destination[i]);
+	}
+	
 	sha256.Reset();
 	sha256.Write((const unsigned char*)destination, 32);
 	sha256.Finalize((unsigned char*)destination);
 	
+	printf("\r\nFinal SHA256 is:\r\n");
+	for (int i=0; i<32; i++)
+	{
+		printf("%02X", destination[i]);
+	}
+	
 	unsigned char* scryptScratchPad = (unsigned char*)malloc(131583);
 	scrypt_1024_1_1_256_sp_sse2((const char*)source, (char*)scryptHash, (char*)scryptScratchPad);
 	free(scryptScratchPad);
+	
+	printf("\r\nScrypt hash is:\r\n");
+	for (int i=0; i<32; i++)
+	{
+		printf("%02X", scryptHash[i]);
+	}
+
 	for (int i=0; i<32; i++)
 	{
 		destination[i] ^= scryptHash[i];
+	}
+	
+	printf("\r\nScrypt^(SHA256^2) is:\r\n");
+	for (int i=0; i<32; i++)
+	{
+		printf("%02X", destination[i]);
 	}
 	
 	// Xor with Kek or Shadilay, effectively requiring Kek or Shadilay to be written at the end of the xor'd hashes
@@ -41,5 +66,11 @@ void runKekHash(char* source, int length, char* destination)
 		{
 			destination[31-i] ^= strKek[i%sizeof(strKek)];
 		}
+	}
+	
+	printf("\r\nAfter kek/shadilay overlay is:\r\n");
+	for (int i=0; i<32; i++)
+	{
+		printf("%02X", destination[i]);
 	}
 }
